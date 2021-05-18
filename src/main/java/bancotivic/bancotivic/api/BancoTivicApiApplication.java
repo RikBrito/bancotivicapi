84d32c5e5893f5adf6f5fc2b74142c8ef4a72094
RikBrito <riksbrito@gmail.com>
2021-05-18 16:09:02 -0300

    message: api

---
+++ src/main/java/bancotivic/bancotivic/api/BancoTivicApiApplication.java
@@ -0,0 +1,13 @@
+package bancotivic.bancotivic.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoTivicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoTivicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bancotivic/bancotivic/api/BancoTivicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bancotivic.bancotivic.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoTivicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
